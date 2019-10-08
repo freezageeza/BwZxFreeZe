05a4eab4fbed6d45231087f321f21d950d9fd5ba
freezageeza <f-r-e-e-z-a@hotmail.com>
2019-10-08 10:48:17 +0100

    test commit

---
+++ Hi.js
@@ -0,0 +1,12 @@
+const teamCheck = (teamInput) => 
+    {
+        if(teamInput == "watford")
+        {
+        console.Log(`${teamInput} are literally the worst team right now`)
+        } else
+        {
+        console.log(`to be fair anyteam is better than watford at the moment, ${teaminput} are above WATFORD`)
+        }
+    }
+
+    teamCheck("watford")
